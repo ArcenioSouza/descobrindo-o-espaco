6a1f5af68429c2a0069778446365003fc0e3b77f
Arcenio Souza <arcenio_neto@icloud.com>
2021-12-11 17:00:59 -0300

    Finaliza formatação do projeto

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-function getApiApod(){
-   var date = $('#date').val()
+function getApiApod() {
+   var date = $("#date").val();
    $.ajax({
       type: "GET",
-      url: `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${date}`,
+      url: `https://api.nasa.gov/planetary/apod?api_key=1smXQq2I9HaJBSmVAQPc1yT7ylgQyXQEOuMGmUp4&date=${date}`,
+
       success: function (response) {
-         var divExplanation = $('<p></p>').append(response.explanation)
-         var imgEspaco = $('<img src="'+response.url+'">')
-         $('#info-apod').append(imgEspaco)
-        $('#info-apod').append(divExplanation); 
-      }
+         console.log(response)
+         var divExplanation = $('<img src="' +response.url +'"><div id="description"><h1>"' +
+               response.title +
+               '"</h1><p>"' +
+               response.explanation +
+               '"</p></div>'
+         );
+
+         $("#info-apod").html(divExplanation);
+      },
    });
 }
